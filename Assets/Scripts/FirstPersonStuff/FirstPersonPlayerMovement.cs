3e19c6187ce735d1640bb1fd9591b207a1912be1
Tom <heeley.thomas@gmail.com>
2020-04-29 09:30:40 +1200

    First Person Camera Fixed

--- Assets/Scripts/FirstPersonStuff/FirstPersonPlayerMovement.cs
+++ Assets/Scripts/FirstPersonStuff/FirstPersonPlayerMovement.cs
@@ -9,9 +9,11 @@ public class FirstPersonPlayerMovement : MonoBehaviour
     public PlayerControls _controls;
 
     [SerializeField] public float moveSpeed;
+    [SerializeField] public GameObject fpCamera;
 
     // Vectors
     Vector2 Move;
+    Vector3 Rotate;
 
     // Rigid Body For Movement
     private Rigidbody rb;
@@ -23,6 +25,10 @@ public class FirstPersonPlayerMovement : MonoBehaviour
         // Movement
         _controls.Gameplay.Move.performed += ctx => Move = ctx.ReadValue<Vector2>();
         _controls.Gameplay.Move.canceled += ctx => Move = Vector2.zero;
+
+        // Camera Rotation
+        _controls.Gameplay.Rotate.performed += ctx => Rotate = ctx.ReadValue<Vector2>();
+        _controls.Gameplay.Rotate.canceled += ctx => Rotate = Vector2.zero;
     }
 
     private void OnEnable()
@@ -41,7 +47,12 @@ public class FirstPersonPlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        fpCamera.GetComponent<FirstPersonCamera>().currentX -= Rotate.y * 2.0f;
+        fpCamera.GetComponent<FirstPersonCamera>().currentY += Rotate.x * 2.0f;
+
+        transform.rotation = Quaternion.Euler(0, fpCamera.GetComponent<FirstPersonCamera>().currentY, 0);
         Vector3 MoveDirection = (transform.right * moveSpeed * Move.x) + (transform.forward * moveSpeed  * Move.y);
+        rb.velocity = new Vector3(MoveDirection.x, rb.velocity.y, MoveDirection.z);
     }
 
 
