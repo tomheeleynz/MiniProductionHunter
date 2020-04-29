8def1362417032e5275819039fd82051ffd51c0f
Gexipas <49463319+Gexipas@users.noreply.github.com>
2020-04-29 17:28:43 +1200

    bear ai on land

--- Assets/Scripts/BearAI.cs
+++ Assets/Scripts/BearAI.cs
@@ -1,13 +1,19 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class BearAI : MonoBehaviour
-{
+{    enum bearMovement
+    {
+        Idle,
+        Walking,
+        Running
+    }
+
     // Variables    
     public int bearHp = 5;
 
-    public GameObject cyc;
     public GameObject player;
 
     public float distanceWander = 4.0f;
@@ -22,7 +28,7 @@ public class BearAI : MonoBehaviour
 
     bearMovement currentMovement = bearMovement.Walking;
     NavMeshAgent agent;
-
+    
     bool attack = false;
     float timerAttack = 0;
     float tempSpeed = 7f;
@@ -33,24 +39,20 @@ public class BearAI : MonoBehaviour
     float timerIdle = 0;
     float idleDuration;
 
-    enum bearMovement
-    {
-        Idle,
-        Walking,
-        Running
-    }
+    
 
     void Start()
     {
         agent = gameObject.GetComponent<NavMeshAgent>();
+        SwitchMovement(bearMovement.Walking);
     }
 
     void Update()
     {
-        if(attack)
+        if (attack)
         {
             timerAttack += Time.deltaTime;
-            if(timerAttack > attackDuration)
+            if (timerAttack > attackDuration)
             {
                 runningSpeed = tempSpeed;
                 attack = false;
@@ -76,7 +78,7 @@ public class BearAI : MonoBehaviour
         {
             SwitchMovement(bearMovement.Idle);
         }
-        if(currentMovement == bearMovement.Idle)
+        if (currentMovement == bearMovement.Idle)
         {
             gameObject.transform.Rotate(0, degreeAdd, 0, Space.Self);
             degreeMove -= degreeAdd;
@@ -93,7 +95,8 @@ public class BearAI : MonoBehaviour
             }
         }
 
-        if (Vector3.Angle(transform.forward, player.transform.position - transform.position)< totalFOV/2)
+        if (Vector3.Angle(transform.forward, player.transform.position - transform.position) < totalFOV / 2 &&
+            Vector3.Distance(player.transform.position, transform.position) < 20)
         {
             RaycastHit hit;
             Ray ray = new Ray(transform.position, player.transform.position - transform.position);
@@ -107,7 +110,14 @@ public class BearAI : MonoBehaviour
                     SwitchMovement(bearMovement.Running);
                 }
             }
-        }        
+        }
+        print("test");
+       //NavMeshHit hitt;
+       //if (NavMesh.FindClosestEdge(transform.position, out hitt, NavMesh.AllAreas) && Vector3.Distance(hitt.position, transform.position) < 4)
+       //{
+       //    moveTo += (transform.position - hitt.position)*0.01f;
+       //    agent.SetDestination(moveTo);
+       //}
     }
 
     Vector3 RandomNavSphere(Vector3 origin, float radius)
@@ -130,6 +140,7 @@ public class BearAI : MonoBehaviour
             }
         }  
     }
+    
 
     
 
@@ -162,13 +173,110 @@ public class BearAI : MonoBehaviour
                 {
                     agent.speed = walkingSpeed;
                     moveTo = RandomNavSphere(transform.position + (transform.forward * distanceWander), radiusWander);
-                    cyc.transform.position = moveTo;
+                    
                     agent.SetDestination(moveTo);
                     break;
                 }
         }
     }
+    /*
 
+    public float speed = 3;
+    public float directionChangeInterval = 1;
+    public float maxHeadingChange = 30;
+
+    float heading;
+    Vector3 targetRotation;
+
+    void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    void Awake()
+    {
+
+        // Set random initial rotation
+        heading = Random.Range(0, 360);
+        transform.eulerAngles = new Vector3(0, heading, 0);
+
+        StartCoroutine(NewHeading());
+    }
+
+    void Update()
+    {
+        transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, targetRotation, Time.deltaTime * directionChangeInterval);
+        var forward = transform.forward;
+        agent.Move(forward * Time.deltaTime * speed);
+    }
+
+    /// <summary>
+    /// Repeatedly calculates a new direction to move towards.
+    /// Use this instead of MonoBehaviour.InvokeRepeating so that the interval can be changed at runtime.
+    /// </summary>
+    IEnumerator NewHeading()
+    {
+        while (true)
+        {
+            NewHeadingRoutine();
+            yield return new WaitForSeconds(directionChangeInterval);
+        }
+    }
+
+    /// <summary>
+    /// Calculates a new direction to move towards.
+    /// </summary>
+    void NewHeadingRoutine()
+    {
+        NavMeshHit hit;
+        float floor = heading;
+        float ceil = heading;
+        if (NavMesh.FindClosestEdge(transform.position, out hit, NavMesh.AllAreas) && Vector3.Distance(hit.position, transform.position) < 8)
+        {
+            cyc.transform.position = hit.position;
+            float x = Quaternion.LookRotation(hit.position - transform.position, Vector3.up).eulerAngles.y;
+            print(x-heading);
+            if(x-heading == 0)
+            {
+                floor = Mathf.Clamp(heading - maxHeadingChange, 0, 360);
+                ceil = Mathf.Clamp(heading + maxHeadingChange, 0, 360);
+                if (Random.Range(0, 2) == 0)
+                {
+                    heading = floor;
+                }
+                else
+                {
+                    heading = ceil;
+                }
+            }
+            else
+            {
+                if(x-heading > 0)
+                {
+                    floor = Mathf.Clamp(heading - maxHeadingChange, 0, 360);
+                }
+                else
+                {
+                    ceil = Mathf.Clamp(heading + maxHeadingChange, 0, 360);
+                }
+                
+                
+                heading = Random.Range(floor, ceil);
+            }    
+        }
+        else
+        {
+            floor = Mathf.Clamp(heading - maxHeadingChange, 0, 360);
+            ceil = Mathf.Clamp(heading + maxHeadingChange, 0, 360);
+            heading = Random.Range(floor, ceil);
+        }
+
+        
+        
+        targetRotation = new Vector3(0, heading, 0);
+    }
+
+    */
     void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
@@ -184,4 +292,5 @@ public class BearAI : MonoBehaviour
         Gizmos.DrawRay(transform.position + rightRayDirection * rayRange, (transform.position + leftRayDirection * rayRange) - (transform.position + rightRayDirection * rayRange));
         
     }
+    
 }
